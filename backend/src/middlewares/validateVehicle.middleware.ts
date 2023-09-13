66eadaf16cdd684473d3de5611cc43cdebdb2352
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-09-12 21:52:59 -0300

    feat: vehicle validation middleware

---
+++ backend/src/middlewares/validateVehicle.middleware.ts
@@ -0,0 +1,50 @@
+import { NextFunction, Request, Response } from "express";
+import customError from "../errors/error";
+
+const validateVehicle = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { modelo, anoDeFabricacao, quantidadeDePortas, marca, passageiros, vehicleType } = req.body;
+
+    validateField(modelo, 'string', 'modelo');
+    validateField(anoDeFabricacao, 'string', 'anoDeFabricacao')
+    validateField(marca, 'string', 'marca')
+    validateField(vehicleType, 'string', 'vehicleType')
+
+    if (vehicleType === 'car') {
+      validateField(quantidadeDePortas, 'number', 'quantidadeDePortas')
+    } else if (vehicleType === 'motorcycle') {
+      validateField(passageiros, 'number', 'passageiros')
+    } else {
+      throw customError({
+        name: 'BAD_REQUEST',
+        statusCode: 400,
+        message: 'O tipo de veículo só pode ser car ou motorcycle.'
+      })
+    }
+
+    next();
+  
+  } catch(error) {
+    next(error);
+  }
+}
+
+const validateField = <T>(value: T, type: string, fieldName: string) => {
+  if (value === undefined || value === null) {
+    throw customError({
+      name: 'VALIDATION_ERROR',
+      statusCode: 400,
+      message: `${fieldName} é um campo obrigatório.`
+    })
+  }
+
+  if (typeof value !== type) {
+    throw customError({
+      name: 'VALIDATION_ERROR',
+      statusCode: 400,
+      message: `O tipo de ${fieldName} é invalido.`
+    })
+  }
+}
+
+export default validateVehicle;
--- backend/src/routes/vehicle.routes.ts
+++ backend/src/routes/vehicle.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import vehicleController from "../controller/vehicle.controller";
+import validateVehicle from "../middlewares/validateVehicle.middleware";
 
 const router = Router();
 
 router.get('/vehicle', vehicleController.getVehicles);	
-router.post('/vehicle', vehicleController.createVehicles);
+router.post('/vehicle', validateVehicle, vehicleController.createVehicles);
 
 export default router;
