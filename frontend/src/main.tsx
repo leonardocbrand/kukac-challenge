0691a3c65bdb420567787033672958e65964d6cf
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-09-12 22:34:13 -0300

    chore: add react tostify, queryClient, axios and MUI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './services/queryClient.ts'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={ queryClient }>
+      <ToastContainer />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
---
+++ frontend/src/services/queryClient.ts
@@ -0,0 +1,13 @@
+import { QueryClient } from "@tanstack/react-query";
+
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 5 * 60 * 1000
+    }
+  }
+})
